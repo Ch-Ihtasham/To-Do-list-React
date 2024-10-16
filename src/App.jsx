807397e0b66e60f60b95bdef5129a94e2795e761
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-16 13:13:50 +0500

    check

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,42 @@ function App() {
     setNewTask(e.target.value)
   }
   const handleAdd = () => {
-    setTasks([...tasks, { text: newTask, isDone: false }]);
-    setNewTask('')
+    if (newTask.trim() != '') {
+      setTasks([...tasks, { text: newTask, isDone: false }]);
+      setNewTask('')
+    }
+  }
+  function handelDone(index) {
+    const updateTasks = tasks.map((v, i) =>
+      i === index ? { ...v, isDone: !v.isDone } : v
+    )
+    setTasks(updateTasks)
+
   }
 
   return (
     <div>
       <div>
-        <input type="text" onChange={handleChange} value={newTask} />
-        <button onClick={handleAdd} >Add</button>
+        <input type="text" onChange={handleChange} value={newTask} className='border-2 border-black' />
+        <button onClick={handleAdd} className='' >Add</button>
       </div>
       <div>
-        <h2>your task</h2>
-        {tasks.map((v, i) => (
-          <div key={i}>{v.text}</div>
+        <h2 className=''>your task</h2>
+        {tasks.map((task, index) => (
+          <div key={index}>
+            <div className='flex justify-evenly w-80'>
+              <input type="checkbox"
+                checked={task.isDone}
+                onChange={() => handelDone(index)}
+
+              />
+              <div className={task.isDone ? 'line-through' : ''} >{task.text}</div>
+              <div className='flex gap-4'>
+                <button>edit</button>
+                <button>remove</button>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
