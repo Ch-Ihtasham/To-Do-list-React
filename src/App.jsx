c8a308e0db4aa39012f68c364c5844fafca6149e
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-16 12:13:50 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import { useState } from 'react'
 
 
 function App() {
-
+  const [tasks, setTasks] = useState([]); // Array to store tasks
+  const [newTask, setNewTask] = useState(''); // Store new task input
+  const handleChange = (e) => {
+    setNewTask(e.target.value)
+  }
+  const handleAdd = () => {
+    setTasks([...tasks, { text: newTask, isDone: false }]);
+    setNewTask('')
+  }
 
   return (
     <div>
       <div>
-        <input type="text" />
-        
-        <button>Add</button>
+        <input type="text" onChange={handleChange} value={newTask} />
+        <button onClick={handleAdd} >Add</button>
+      </div>
+      <div>
+        <h2>your task</h2>
+        {tasks.map((v, i) => (
+          <div key={i}>{v.text}</div>
+        ))}
       </div>
     </div>
   )
